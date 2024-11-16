31032ff913e4ea0218b4b682e2a39ea5ea2122aa
tdemagneval <thomas.magneval@gmail.com>
2024-11-16 22:48:29 +0100

    Corrcted

--- Front_End/pages/search.py
+++ Front_End/pages/search.py
@@ -1,13 +1,4 @@
 import streamlit as st
-from navigation import render_navigation_bar
 
-def app():
-    # Render navigation bar
-    render_navigation_bar()
-
-    # Search page content
-    st.markdown(
-        
-    )
-    st.title("Search Page")
-    st.write("This is where users can search for items.")
+st.set_page_config(layout="wide")
+st.write("Here we'll be able to do searchs!")
